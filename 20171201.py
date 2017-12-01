e7b1006beacbc1e52dc0a4b72202f452f1cafea1
iwi <iwi@tuta.io>
2017-12-01 19:38:28 +0000

    feat: Add second part of day one

--- 20171201.py
+++ 20171201.py
@@ -21,3 +21,24 @@ repeats = list(map((lambda x, y: (int(y) if x else 0)), number_list_diff, origin
 total = sum(repeats)
 
 print(total)
+
+
+# second part
+
+list_length = len(number_list)
+
+number_list = original[:]
+
+first_part = number_list[0:int(list_length / 2)]
+second_part = number_list[int(list_length/2):list_length]
+
+composite = second_part + first_part
+
+number_list_diff = list(map((lambda x, y: x == y), original, composite))
+
+repeats = list(map((lambda x, y: (int(y) if x else 0)), number_list_diff, original))
+
+total = sum(repeats)
+
+print(total)
+
