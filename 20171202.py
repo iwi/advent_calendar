1f239661204393e6f7c20a2720b82dd3e1ee1134
iwi <iwi@tuta.io>
2017-12-02 16:59:46 +0000

    feat: Add part two

--- 20171202.py
+++ 20171202.py
@@ -15,7 +15,30 @@ def checksum(input_csv):
     return(sum(diffs))
 
 
+# Part two
+def find_pairs_division(row):
+    row.sort(reverse= True)
+    div = [int(num / den) for i, num in enumerate(row) for den in row[i + 1:len(row)] if num % den == 0]
+    return(div)
+
+
+def divisions(input_csv):
+    divs = []
+    for row in input_csv:
+        div = find_pairs_division(row)
+        divs += div
+    return(divs)
+
+def checksum_div(input_csv):
+    divs = divisions(input_csv)
+    return(sum(divs)) 
+
+
 if __name__ == '__main__':
     with open('input_data_20171202.csv', 'r') as csvfile:
         input = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
         print(checksum(input)) 
+
+    with open('input_data_20171202.csv', 'r') as csvfile:
+        input = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+        print(checksum_div(input))
