5b9e3b956d40ca4f85dc30bc4f61ab144e550a1d
iwi <iwi@tuta.io>
2017-12-06 13:53:04 +0000

    feat: Add 5.2 solution

--- day_20171205.py
+++ day_20171205.py
@@ -24,13 +24,32 @@ def count_steps_to_exit(maze):
     return steps
 
 
+def count_steps_to_exit_2(maze):
+    location = 0
+    steps = 0
+    ll = [location]
+    while (location < len(maze)) & (location >= 0):
+        change = int(maze[location][0])
+        if change < 3:
+            maze[location][0] += 1
+        else:
+            maze[location][0] -= 1
+        location = location + change
+        ll.append(location)
+        steps += 1
+
+    print(ll)
+    return steps
+
+
 if __name__ == '__main__':
     maze = []
     with open('data/input_data_20171205.csv', 'r') as csvfile:
         data = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
         for element in data:
             maze.append(element)
-        steps = count_steps_to_exit(maze)
-        print('steps: ', steps)
-        print('trial steps: ', count_steps_to_exit([[0], [3], [0], [1], [-3]]))
 
+        print('steps: ', count_steps_to_exit(maze))
+        print('steps: ', count_steps_to_exit_2(maze))
+        print('trial steps: ', count_steps_to_exit([[0], [3], [0], [1], [-3]]))
+        print('trial steps 2: ', count_steps_to_exit_2([[0], [3], [0], [1], [-3]]))
