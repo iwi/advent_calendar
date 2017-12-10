2a83e7cdbe3aeb20030aabc17c007676a8dddcf5
iwi <iwi@tuta.io>
2017-12-10 21:00:04 +0000

    tests: Add tests for zero

--- tests/test_day10.py
+++ tests/test_day10.py
@@ -6,6 +6,13 @@ def test_hash():
     sequence = list(range(5))
     assert ten.hash(sequence, lengths) == [3, 4, 2, 1, 0]
 
+    lengths = [0]
+    sequence = list(range(5))
+    assert ten.hash(sequence, lengths) == [0, 1, 2, 3, 4]
+
+    lengths = [7]
+    sequence = list(range(5))
+    assert ten.hash(sequence, lengths) == [1, 0, 4, 3, 2]
 
 def test_get_subsequence():
     sequence = [0, 1, 2, 3, 4]
@@ -28,19 +35,22 @@ def test_substitute_sequence():
     sequence = [0, 1, 2, 3, 4]
     start = 3
     end = 1
-    assert ten.substitute_subsequence(subsequence, sequence, start, end) == [4, 3, 2, 1, 0]
+    length = 4
+    assert ten.substitute_subsequence(subsequence, sequence, start, end, length) == [4, 3, 2, 1, 0]
 
     subsequence = [3, 2, 1]
     sequence = [0, 1, 2, 3, 4]
     start = 1
     end = 3
-    assert ten.substitute_subsequence(subsequence, sequence, start, end) == [0, 3, 2, 1, 4]
+    length = 3
+    assert ten.substitute_subsequence(subsequence, sequence, start, end, length) == [0, 3, 2, 1, 4]
 
     subsequence = [4, 2, 1, 0, 3]
     sequence = [4, 3, 0, 1, 2]
     start = 1
     end = 0
-    assert ten.substitute_subsequence(subsequence, sequence, start, end) == [3, 4, 2, 1, 0]
+    length = 5
+    assert ten.substitute_subsequence(subsequence, sequence, start, end, length) == [3, 4, 2, 1, 0]
 
 
 def test_apply_length():
@@ -79,7 +89,20 @@ def test_apply_length():
             'skip_size': 4,
             'sequence': [3, 4, 2, 1, 0]}
     assert ten.apply_length(length, status) == transformed_status
-    
+
+    length = 0
+    sequence = [0, 1, 2, 3, 4]
+    status = {
+            'position': 0,
+            'skip_size': 0,
+            'sequence': sequence}
+    transformed_status = {
+            'position': 0,
+            'skip_size': 1,
+            'sequence': sequence}
+    assert ten.apply_length(length, status) == transformed_status
+
+
 def test_checksum():
     hashed_sequence = [4, 3, 0, 1, 2]
     assert ten.checksum(hashed_sequence) == 12
