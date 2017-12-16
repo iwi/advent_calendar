6dcf3ff7bf45aa2eb130c91145369801a278bf71
iwi <iwi@tuta.io>
2017-12-16 19:40:19 +0000

    feat: Add first attempt of part 2

--- day_20171214.py
+++ day_20171214.py
@@ -14,6 +14,50 @@ def build_input_strings(st):
     return built_strings
 
 
+def convert_filled_adjacent(row, col, bin_rows_list):
+    print('pos (', row, ', ', col, ')')
+    if (row == len(bin_rows_list)) | (col == len(bin_rows_list[0])) | (col < 0):
+        print('bang!')
+        return bin_rows_list
+
+    print('bin_rows_list[r,c]', bin_rows_list[row][col]) 
+    
+    if bin_rows_list[row][col] == 0:
+        print('zero!')
+        return bin_rows_list
+
+    bin_rows_list[row][col] = 0
+
+    bin_rows_list = convert_filled_adjacent(row + 1,
+                                            col,
+                                            bin_rows_list)
+    print(bin_rows_list)
+    bin_rows_list = convert_filled_adjacent(row,
+                                            col + 1,
+                                            bin_rows_list)
+    bin_rows_list = convert_filled_adjacent(row,
+                                            col - 1,
+                                            bin_rows_list)
+
+    return bin_rows_list
+    
+
+def count_groups(bin_rows_list):
+    num_of_groups = 0
+    nrows = len(bin_rows_list)
+    ncols = len(bin_rows_list[0])
+    for row in range(nrows):
+        for col in range(ncols):
+            element = bin_rows_list[row][col]
+            if element == 1:
+                num_of_groups += 1
+                bin_rows_list[row][col] = 0
+                num_of_groups = count_filled_adjacent(row, col,
+                                                        bin_rows_list,
+                                                        num_of_groups)
+    return num_of_groups
+
+
 if __name__ == '__main__':
     input_string = 'nbysizxe'
     # input_string = 'flqrgnkx'
@@ -21,6 +65,8 @@ if __name__ == '__main__':
     hashed_rows = ['0x' + dt.hash(s) for s in input_strings]
     bin_rows = [bin(int(x, 16))[2:] for x in hashed_rows]
     suma = reduce(lambda i, bs: i + bs.count('1'), bin_rows, 0) 
-    print(suma)
+    print('sum: ', suma)
 
-
+    bin_rows_list = [list(row) for row in bin_rows]
+    number_of_groups = count_groups(bin_rows)
+    print(number_of_groups)
